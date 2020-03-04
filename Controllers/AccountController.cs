9ab1585aaeeed0e19e03c405b6b0f3cd455235c4
Manny <mannym2124@gmail.com>
2020-03-03 19:55:46 -0700

    initial commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace unity_course_1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string Username, string Password)
+        {
+            if(Username == "admin" && Password == "manager")
+            {
+                return RedirectToAction("Dashboard", "Admin"); // action, controller
+            }
+            else
+            {
+                return RedirectToAction("InvalidLogin");
+            }
+
+            //return View();
+        }
+
+        public ActionResult InvalidLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="unity_course_1.MvcApplication" Language="C#" %>
